803424e4d7075f45904e8d5ede82584c0f4e788a
khs <khs@meh.(none)>
2011-04-13 11:40:42 +0200

    fixed source for report

--- program/build/animate.sh
+++ program/build/animate.sh
@@ -6,5 +6,7 @@ obr=1200
 opt="mbd=2:keyint=132:cmp=2:subcmp=2:dia=2:mv0:last_pred=3"
 codec="msmpeg4v2"
 
-mencoder -ovc lavc -lavcopts vcodec=$codec:vbitrate=$obr:vpass=1:$opt:turbo -nosound -mf fps=$fps:type=png   -o NUL: mf://data/*.png
-mencoder -ovc lavc -lavcopts vcodec=$codec:vbitrate=$obr:vpass=2:$opt -nosound -mf fps=$fps:type=png -o $file_out mf://data/*.png
+mencoder -ovc lavc -lavcopts vcodec=$codec:vbitrate=$obr:vpass=1:$opt:turbo \
+-nosound -mf fps=$fps:type=png   -o NUL: mf://data/*.png
+mencoder -ovc lavc -lavcopts vcodec=$codec:vbitrate=$obr:vpass=2:$opt \
+-nosound -mf fps=$fps:type=png -o $file_out mf://data/*.png
